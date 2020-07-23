1f239d4570f9d44b88f139a8b07a189299166b34
DaveShedar <62851456+DaveShedar@users.noreply.github.com>
2020-07-23 14:20:17 +0300

    .

--- src/main/java/ddd/Spring_Boot_CRUD/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ddd.Spring_Boot_CRUD.repository;
-
-import ddd.Spring_Boot_CRUD.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/ddd/Spring_Boot_CRUD/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ddd.Spring_Boot_CRUD.repository;
-
-
-import ddd.Spring_Boot_CRUD.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository< User, Long> {
-    User findByUsername(String username);
-}
